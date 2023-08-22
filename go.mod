c88897d0127468535cd5c76ca62e34fda47fe283
Bernd Warmuth <72415058+warber@users.noreply.github.com>
2023-08-22 13:07:02 +0200

    feat: Add functions for creation of authorized HTTP clients (#8)

--- go.mod
+++ go.mod
@@ -5,11 +5,16 @@ module github.com/dynatrace/dynatrace-configuration-as-code-core
 require (
 	github.com/go-logr/logr v1.2.4
 	github.com/stretchr/testify v1.8.4
+	golang.org/x/oauth2 v0.11.0
 	golang.org/x/time v0.3.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/api/auth/token.go
@@ -0,0 +1,63 @@
+// @license
+// Copyright 2023 Dynatrace LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import "net/http"
+
+// NewTokenBasedClient creates a new HTTP client with token-based authentication.
+// It takes a token string as an argument and returns an instance of *http.Client.
+func NewTokenBasedClient(token string) *http.Client {
+	// Create a new tokenAuthTransport and initialize it with the provided token.
+	return &http.Client{Transport: newTokenAuthTransport(nil, token)}
+}
+
+// tokenAuthTransport is a custom transport that adds token-based authentication headers to HTTP requests.
+type tokenAuthTransport struct {
+	http.RoundTripper
+	header http.Header
+}
+
+// newTokenAuthTransport creates a new instance of tokenAuthTransport.
+// It takes a baseTransport (an existing HTTP transport) and a token string as arguments,
+// and returns a pointer to the newly created tokenAuthTransport instance.
+func newTokenAuthTransport(baseTransport http.RoundTripper, token string) *tokenAuthTransport {
+	// If no baseTransport is provided, use the default HTTP transport.
+	if baseTransport == nil {
+		baseTransport = http.DefaultTransport
+	}
+
+	// Create a new tokenAuthTransport instance and initialize it.
+	t := &tokenAuthTransport{
+		RoundTripper: baseTransport,
+		header:       http.Header{},
+	}
+
+	// Set the "Authorization" header with the provided token.
+	t.header.Set("Authorization", "Api-Token "+token)
+	return t
+}
+
+// RoundTrip implements the http.RoundTripper interface's RoundTrip method.
+// It adds the authentication headers from the tokenAuthTransport instance to the request's headers
+// and delegates the actual round trip to the underlying transport.
+func (t *tokenAuthTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Copy authentication headers from tokenAuthTransport to the request.
+	for k, v := range t.header {
+		req.Header[k] = v
+	}
+
+	// Perform the actual HTTP request using the underlying transport.
+	return t.RoundTripper.RoundTrip(req)
+}
