02160b1ad7238dec185be3c42162d08afc3c0d5a
Riedmann <nicola.riedmann@dynatrace.com>
2023-08-29 15:51:36 +0200

    feat(api): Introduce ListResponse and function for decoding all contained objects
    Similar to the existing api.Response an api.ListResponse is added and used in bucket.ListResponse.
    For convenience a generic DecodeJSONObjects function is added to allow decoding the contained list
    of JSON into struct objects.
    To make this easily usable the function is generic, just requiring a caller to provide the desired
    type, instead of a prefilled slice with objects of the type to write into.
    Sadly methods can not be generic, so unlike DecodeJSON the list version can not be a method of the
    ListResponse struct.

--- internal/api/response.go
+++ internal/api/response.go
@@ -25,22 +25,29 @@ type Response struct {
 	Data       []byte `json:"-"`
 }
 
+// ListResponse represents a multi-object API response
+// It contains both the full JSON Data, and a slice of Objects for more convenient access
+type ListResponse struct {
+	Response
+	Objects [][]byte `json:"-"`
+}
+
 // IsSuccess returns true if the response indicates a successful HTTP status code.
 // A status code between 200 and 299 (inclusive) is considered a success.
-func (resp Response) IsSuccess() bool {
-	return resp.StatusCode >= 200 && resp.StatusCode <= 299
+func (r Response) IsSuccess() bool {
+	return r.StatusCode >= 200 && r.StatusCode <= 299
 }
 
 // Is4xxError returns true if the response indicates a 4xx client error HTTP status code.
 // A status code between 400 and 499 (inclusive) is considered a client error.
-func (resp Response) Is4xxError() bool {
-	return resp.StatusCode >= 400 && resp.StatusCode <= 499
+func (r Response) Is4xxError() bool {
+	return r.StatusCode >= 400 && r.StatusCode <= 499
 }
 
 // Is5xxError returns true if the response indicates a 5xx server error HTTP status code.
 // A status code between 500 and 599 (inclusive) is considered a server error.
-func (resp Response) Is5xxError() bool {
-	return resp.StatusCode >= 500 && resp.StatusCode <= 599
+func (r Response) Is5xxError() bool {
+	return r.StatusCode >= 500 && r.StatusCode <= 599
 }
 
 // DecodeJSON tries to unmarshal the data of the response to obj
@@ -49,4 +56,19 @@ func (r Response) DecodeJSON(obj interface{}) error {
 		return fmt.Errorf("failed to unmarshal JSON: %w", err)
 	}
 	return nil
+}
+
+// DecodeJSONObjects unmarshalls the Objects contained in the given ListResponse into a slice of T.
+// To decode the full JSON response contained in ListResponse use Response.DecodeJSON.
+func DecodeJSONObjects[T any](r ListResponse) ([]T, error) {
+	res := make([]T, len(r.Objects))
+	for i, o := range r.Objects {
+		var t T
+		if err := json.Unmarshal(o, &t); err != nil {
+			return []T{}, fmt.Errorf("failed to unmarshal JSON: %w", err)
+		}
+		res[i] = t
+	}
+
+	return res, nil
 }
