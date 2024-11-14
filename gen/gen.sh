be255db0a74c59959f7b312806e54325b8b4c919
Arthur Pitman <arthur.pitman@dynatrace.com>
2024-11-14 13:00:38 +0100

    chore: Update gen script

--- gen/gen.sh
+++ gen/gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export GO_POST_PROCESS_FILE="gofmt -w"
-openapi-generator generate -i https://api.dynatrace.com/spec-json -g go -t template -o account_management --remove-operation-id-prefix --enable-post-process-file --additional-properties=packageName=accountmanagement
+openapi-generator generate -i ./specs/account_management/spec_formatted_fixed.json -g go -t template -o account_management --remove-operation-id-prefix --enable-post-process-file --additional-properties=packageName=accountmanagement,disallowAdditionalPropertiesIfNotPresent=false
