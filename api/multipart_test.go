eabb794a11319590940e1e4129135e49eea6624d
Tomaž <32056440+tomazpu@users.noreply.github.com>
2025-03-28 13:11:48 +0100

    fix: add test to cover `MultipartResponse` in `api` package (#199)
    <!--  Thanks for sending a pull request! 
    If this is your first time, please read our contributor guidelines:
    https://github.com/Dynatrace/dynatrace-configuration-as-code-core/blob/main/CONTRIBUTING.md
    
    Before submitting this PR, please make sure that:
    1. Your code builds without any errors or warnings
    2. Your code is covered by unit tests
    3. Your branch is rebased on top of current main (`git pull --rebase
    origin main`)
    -->
    
    #### What this PR does / Why we need it:
    add test to cover `MultipartResponse` in `api` package
    #### Special notes for your reviewer:
    
    #### Does this PR introduce a user-facing change?
    No, user changes.
    <!--
    If no, just write "NONE" or leave this section empty.
    If yes, state how the user is impacted by your changes.
    -->

--- api/multipart_test.go
+++ api/multipart_test.go
@@ -17,16 +17,69 @@
 package api_test
 
 import (
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/dynatrace/dynatrace-configuration-as-code-core/api"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNewMultipartResponse(t *testing.T) {
+	partOne := api.Part{
+		FormName: "metadata",
+		FileName: "metadata.json",
+		Content:  []byte(`{"key":"value"}`),
+	}
+
+	request := http.Request{
+		Method: http.MethodGet,
+		URL: &url.URL{
+			Scheme: "http",
+			Host:   "something.com",
+			Path:   "/metadata",
+		},
+	}
+
+	response := http.Response{
+		StatusCode: http.StatusOK,
+		Request:    &request,
+		Body:       io.NopCloser(strings.NewReader(`{"key":"value"}`)),
+	}
+
+	multiResponse := api.NewMultipartResponse(&response, partOne)
+	assert.Equal(t, multiResponse.Request.Method, request.Method)
+	assert.Equal(t, multiResponse.Request.URL, request.URL.String())
+	assert.Equal(t, len(multiResponse.Parts), 1)
+	assert.Equal(t, multiResponse.Parts[0], partOne)
+	assert.Equal(t, multiResponse.StatusCode, response.StatusCode)
+}
+
 func TestMultipartResponse_IsSuccess(t *testing.T) {
 	t.Run("invoke over null object", func(t *testing.T) {
 		var mr *api.MultipartResponse
 
 		assert.False(t, mr.IsSuccess())
+	})
+}
+
+func TestMultipartResponse_GetPartWithName(t *testing.T) {
+	t.Run("part found", func(t *testing.T) {
+		expectedPart := api.Part{FormName: "metadata"}
+		meta := api.MultipartResponse{Parts: []api.Part{expectedPart}}
+
+		actualPart, found := meta.GetPartWithName("metadata")
+		assert.True(t, found)
+		assert.Equal(t, actualPart, expectedPart)
+	})
+	t.Run("part missing", func(t *testing.T) {
+		part := api.Part{FormName: "hello"}
+		meta := api.MultipartResponse{Parts: []api.Part{part}}
+
+		actualPart, found := meta.GetPartWithName("metadata")
+		assert.False(t, found)
+		assert.Equal(t, actualPart, api.Part{})
 	})
 }
