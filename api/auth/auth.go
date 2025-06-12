75e948aa5873bc8dd53e568e3f8a0217f8f586d2
David Laubreiter <3740651+Laubi@users.noreply.github.com>
2025-06-12 15:08:14 +0200

    refactor: Improve documentation and streamline function names

--- api/auth/auth.go
+++ api/auth/auth.go
@@ -24,23 +24,31 @@ import (
 	"golang.org/x/oauth2/clientcredentials"
 )
 
-func NewApiTokenBasedClient(ctx context.Context, apiToken string) *http.Client {
+const DynatraceSSOTokenURL = "https://sso.dynatrace.com/sso/oauth2/token" //nolint:gosec
+
+// NewAPITokenClient creates a new [http.Client] that sets the Authentication header to use [Dynatrace API tokens].
+//
+// [Dynatrace API tokens]: https://docs.dynatrace.com/docs/discover-dynatrace/references/dynatrace-api/basics/dynatrace-api-authentication
+func NewAPITokenClient(ctx context.Context, apiToken string) *http.Client {
 	return oauth2.NewClient(ctx, oauth2.StaticTokenSource(&oauth2.Token{TokenType: "Api-Token", AccessToken: apiToken}))
 }
 
+// NewPlatformTokenClient creates a new [http.Client] that sets the Authentication header to use [Dynatrace platform tokens].
+//
+// [Dynatrace platform tokens]: https://docs.dynatrace.com/docs/manage/identity-access-management/access-tokens-and-oauth-clients/platform-tokens
 func NewPlatformTokenClient(ctx context.Context, platformToken string) *http.Client {
 	return oauth2.NewClient(ctx, oauth2.StaticTokenSource(&oauth2.Token{AccessToken: platformToken}))
 }
 
-// NewOAuthBasedClient creates a new HTTP client with OAuth2 client credentials authentication.
-// It takes a context and a clientcredentials.Config as arguments and returns an instance of *http.Client.
-// The client credentials are used to authenticate requests made by the returned HTTP client.
-func NewOAuthBasedClient(ctx context.Context, credentials *clientcredentials.Config) *http.Client {
+// NewOAuthClient creates a new [http.Client] with OAuth2 client credentials authentication.
+// If the [credentials.TokenURL] is not provided, we fall back to the Dynatrace SSO token URL.
+// For more information see the [Dynatrace OAuth client documentation].
+//
+// [Dynatrace OAuth client documentation]: https://docs.dynatrace.com/docs/manage/identity-access-management/access-tokens-and-oauth-clients/oauth-clients
+func NewOAuthClient(ctx context.Context, credentials *clientcredentials.Config) *http.Client {
 	if credentials.TokenURL == "" {
-		credentials.TokenURL = "https://sso.dynatrace.com/sso/oauth2/token" //nolint:gosec
+		credentials.TokenURL = DynatraceSSOTokenURL
 	}
 
-	// Create an HTTP client using the provided OAuth2 client credentials configuration.
-	// This client will automatically manage the token acquisition and inclusion in requests.
 	return credentials.Client(ctx)
 }
