7fb4c95b93f28eac2086426a510cabc33e81f780
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2025-09-03 16:14:21 +0200

    chore: update account management client
    The manual fixes in `spec_formatted_fixed.json` are:
    - removing the `"type": "integer"` from size and page parameters,
    - renaming the `operationId` `"ReferenceDataController_"` to `"ReferenceDataController_getRegions"`,
    - setting `required` parameter to `true` for `InsightsController_getCostAllocationBreakdownPage`,
    - removing `"boundaries"` from the array of required parameters for `Binding`,
    - removing `"groupUuid"` from the array of required parameters for `ExternalServiceUserWithGroupUuidDto`
    - manually adding the response DTO to the `getPolicyBoundary` operation

--- gen/account_management/api_policy_management.go
+++ gen/account_management/api_policy_management.go
@@ -1352,7 +1352,7 @@ func (a *PolicyManagementAPIService) GetEffectivePermissionsExecute(r ApiGetEffe
 		r.page = &defaultValue
 	}
 	if r.services != nil {
-		parameterAddToHeaderOrQuery(localVarQueryParams, "services;", r.services, "form", "")
+		parameterAddToHeaderOrQuery(localVarQueryParams, "services", r.services, "form", "")
 	}
 	parameterAddToHeaderOrQuery(localVarQueryParams, "entityId", r.entityId, "form", "")
 	parameterAddToHeaderOrQuery(localVarQueryParams, "entityType", r.entityType, "form", "")
@@ -2340,7 +2340,7 @@ type ApiGetPolicyBoundaryRequest struct {
 	accountId          interface{}
 }
 
-func (r ApiGetPolicyBoundaryRequest) Execute() (*http.Response, error) {
+func (r ApiGetPolicyBoundaryRequest) Execute() (*PolicyBoundaryOverview, *http.Response, error) {
 	return r.ApiService.GetPolicyBoundaryExecute(r)
 }
 
@@ -2362,16 +2362,19 @@ func (a *PolicyManagementAPIService) GetPolicyBoundary(ctx context.Context, poli
 }
 
 // Execute executes the request
-func (a *PolicyManagementAPIService) GetPolicyBoundaryExecute(r ApiGetPolicyBoundaryRequest) (*http.Response, error) {
+//
+//	@return PolicyBoundaryOverview
+func (a *PolicyManagementAPIService) GetPolicyBoundaryExecute(r ApiGetPolicyBoundaryRequest) (*PolicyBoundaryOverview, *http.Response, error) {
 	var (
-		localVarHTTPMethod = http.MethodGet
-		localVarPostBody   interface{}
-		formFiles          []formFile
+		localVarHTTPMethod  = http.MethodGet
+		localVarPostBody    interface{}
+		formFiles           []formFile
+		localVarReturnValue *PolicyBoundaryOverview
 	)
 
 	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "PolicyManagementAPIService.GetPolicyBoundary")
 	if err != nil {
-		return nil, &GenericOpenAPIError{error: err.Error()}
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
 	}
 
 	localVarPath := localBasePath + "/iam/v1/repo/account/{accountId}/boundaries/{policyBoundaryUuid}"
@@ -2401,19 +2404,19 @@ func (a *PolicyManagementAPIService) GetPolicyBoundaryExecute(r ApiGetPolicyBoun
 	}
 	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
 	if err != nil {
-		return nil, err
+		return localVarReturnValue, nil, err
 	}
 
 	localVarHTTPResponse, err := a.client.callAPI(req)
 	if err != nil || localVarHTTPResponse == nil {
-		return localVarHTTPResponse, err
+		return localVarReturnValue, localVarHTTPResponse, err
 	}
 
 	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
 	localVarHTTPResponse.Body.Close()
 	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
 	if err != nil {
-		return localVarHTTPResponse, err
+		return localVarReturnValue, localVarHTTPResponse, err
 	}
 
 	if localVarHTTPResponse.StatusCode >= 300 {
@@ -2426,15 +2429,24 @@ func (a *PolicyManagementAPIService) GetPolicyBoundaryExecute(r ApiGetPolicyBoun
 			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 			if err != nil {
 				newErr.error = err.Error()
-				return localVarHTTPResponse, newErr
+				return localVarReturnValue, localVarHTTPResponse, newErr
 			}
 			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
 			newErr.model = v
 		}
-		return localVarHTTPResponse, newErr
+		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
 
-	return localVarHTTPResponse, nil
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
 type ApiGetPolicyOverviewListRequest struct {
