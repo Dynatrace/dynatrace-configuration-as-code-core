6dcff06fa0a07d2e948578e7050957ac4cf094de
Arthur Pitman <arthur.pitman@dynatrace.com>
2025-09-29 14:13:49 +0200

    fix: Remove non-existent `actor` field
    The Documents service doesn't support actors and so it can be removed.

--- clients/documents/metadata.go
+++ clients/documents/metadata.go
@@ -1,6 +1,6 @@
 /*
  * @license
- * Copyright 2023 Dynatrace LLC
+ * Copyright 2025 Dynatrace LLC
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -24,7 +24,6 @@ import (
 type Metadata struct {
 	ID                string  `json:"id"`
 	ExternalID        string  `json:"externalId"`
-	Actor             string  `json:"actor"`
 	Owner             string  `json:"owner"`
 	Name              string  `json:"name"`
 	Type              string  `json:"type"`
